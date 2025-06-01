f1002c7c290b822e61134297b9e06bbdf640ec7c
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-05-31 23:54:44 -0600

    docs(rust): add example where stdout/err get processed

--- examples/01_tauri_ws/src-tauri/src/main.rs
+++ examples/01_tauri_ws/src-tauri/src/main.rs
@@ -22,19 +22,25 @@ fn main() {
 
       tauri::async_runtime::spawn(async move {
         while let Some(event) = rx.recv().await {
-          if let CommandEvent::Stdout(line) = event {
-            if line.contains("tauri-server-port=") {
-              let tokens: Vec<&str> = line.split("=").collect();
-              let port_token = tokens[1].to_string();
-              let port = port_token.trim();
-              // println!("window.location.replace(window.location.href + '?sessionURL=ws://localhost:{}/ws')", port);
-              let _ = main_window.eval(&format!("window.location.replace(window.location.href + '?sessionURL=ws://localhost:{}/ws')", port));
-            }
-            if line.contains("tauri-client-ready") {
-              task::sleep(Duration::from_secs(2)).await;
-              splashscreen_window.close().unwrap();
-              main_window.show().unwrap();
-            }
+          match event {
+            CommandEvent::Stdout(line) => {
+              if line.contains("tauri-server-port=") {
+                let tokens: Vec<&str> = line.split("=").collect();
+                let port_token = tokens[1].to_string();
+                let port = port_token.trim();
+                // println!("window.location.replace(window.location.href + '?sessionURL=ws://localhost:{}/ws')", port);
+                let _ = main_window.eval(&format!("window.location.replace(window.location.href + '?sessionURL=ws://localhost:{}/ws')", port));
+              }
+              if line.contains("tauri-client-ready") {
+                task::sleep(Duration::from_secs(2)).await;
+                splashscreen_window.close().unwrap();
+              }
+            },
+            CommandEvent::Stderr(line) => {
+              // Handle stderr output
+              println!("Stderr: {}", line);
+            },
+            _ => {},
           }
         }
       });
